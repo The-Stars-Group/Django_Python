fc734cdf8d295aaf943fbfd6a8cf6a740aed3590
student <Isabellanamutenda>
2020-08-21 17:52:07 +0300

    first commit

--- shipping/models.py
+++ shipping/models.py
@@ -6,4 +6,18 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class ShippingProvider(models.Model):
-    name = models.CharField
+    name = models.CharField(max_length=10)
+    date_joined = models.CharField(max_length=15)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
+    transport_mode = models.CharF(max_length=25)
+
+
+    def __str__(self):
+        return self.shippingprovider()
+
+
+class DispenserCoolerBox(models.Model):      
+     box_number = models.IntegerField()
+     location = models.CharField()
+     unlock_code = models.IntegerField
