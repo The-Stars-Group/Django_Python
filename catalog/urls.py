85d154f2baa4523a29a2b281936c966138690b2e
student <Isabellanamutenda>
2020-09-17 14:12:00 +0300

    first commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import products_list
+from . import views
+from .views import products_list, product_details, upload_product
 
 urlpatterns = [
-    path('products/', products_list, name='products-list'),
+    path('', products_list, name='products-list'),
+    path('products/<int:product_id>/',product_details, name='product-details'),
+    path('products/upload/', upload_product, name='upload-product'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Product 
+from . forms import ProductForm
 
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, 'products_list.html', {'products': products})
+
+
+
+def product_details(request, product_id):
+    products = Product.objects.get(id=product_id)
+    return render(request, 'product_details.html', {'products': products})
+
+def upload_product(request):
+    if request.method == "POST":
+         form = ProductForm(request.POST,request.FILES)
+         if form.is_valid():
+            form.save()
+         return redirect('products_list')
+    else:
+       form = ProductForm()
+       return render(request,'upload_product.html',{'form':form})
 # Create your views here.
